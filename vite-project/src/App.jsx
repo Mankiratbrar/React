afe05dc194e9e001712d6076172bf81e2d7e8617
MankiratBrar <smankirat6u@gmail.com>
2025-07-08 12:01:01 +0530

    --- Auto Git Commit ---

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,12 +1,14 @@
-import UserGreetings from "./UserGreetings";
-
+import List from './List.jsx';
 function App() {
 
+    const fruits=[  {id:1 ,name:  "Apple", calories:95},
+                    {id:2 ,name: "Banana", calories:105},
+                    {id:3 ,name: "Cherry", calories: 50},
+                    {id:4 ,name: "Date" , calories: 282},
+                    {id:5 ,name: "Elderberry",calories: 73}];
+    
   return( 
-    <>
-      <UserGreetings isLoggedIn={true} username="Mankirat Brar" />
-      
-    </>
+        <List items={fruits} category={fruits}/>
       );
 }
 
--- vite-project/src/UserGreetings.jsx
+++
@@ -1,8 +0,0 @@
-function UserGreetings(props) {
-      
-    return (props.isLoggedIn ?
-        <h1 className="greeting">Welcome back, {props.username}!</h1> :
-        <h1 className="warning">Please log in.</h1>
-    );
-}
-export default UserGreetings;
